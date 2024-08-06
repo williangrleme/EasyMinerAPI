e80cb87142f180f810c5edbb468766b2045316ce
Willian Leme <willian_leme@proton.me>
2024-08-05 23:24:15 -0300

    testa rule git

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,6 @@
 import sys
 import os
 
-# Adiciona o diretório do projeto ao sys.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'api')))
 
 from api.app import create_app
