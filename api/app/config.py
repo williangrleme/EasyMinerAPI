229f28cb3a28614d3420963ebfc9f43e7d5d0c39
Willian Leme <willian_leme@proton.me>
2024-08-08 00:43:10 -0300

    (feat): configura todo o crud de datasets para armazenamento no S3 AWS

--- api/app/config.py
+++ api/app/config.py
@@ -7,7 +7,7 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     USER_MASTER = int(os.getenv("USER_MASTER"))
     SWAGGER = {"title": "EasyMinerAPI", "uiversion": 3}
-    UPLOAD_FOLDER = os.getenv(
-        "UPLOAD_FOLDER", default=os.path.join(os.getcwd(), "uploads")
-    )
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # Limite de 16MB
+    S3_KEY = os.getenv("S3_KEY")
+    S3_BUCKET = os.getenv("S3_BUCKET")
+    S3_SECRET = os.getenv("S3_SECRET")
