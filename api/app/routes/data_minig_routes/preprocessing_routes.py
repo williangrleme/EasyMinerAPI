1c7dd9ad9f89bb5eb5a62ff3f650418e62178ff0
Willian Leme <willian_leme@proton.me>
2024-09-11 21:40:28 -0300

    (feat): adiciona roda de redução de dados

--- api/app/routes/data_minig_routes/preprocessing_routes.py
+++ api/app/routes/data_minig_routes/preprocessing_routes.py
@@ -6,6 +6,9 @@ from app.controllers.data_mining.preprocessing.data_cleaning_controller import (
 from app.controllers.data_mining.preprocessing.data_normalization_controller import (
     dataNormalization,
 )
+from app.controllers.data_mining.preprocessing.data_reduction_controller import (
+    dataReduction,
+)
 
 preprocessing_bp = Blueprint("preprocessing", __name__)
 
@@ -14,4 +17,7 @@ preprocessing_bp.route("/data-cleaning/<int:id>", methods=["POST"])(
 )
 preprocessing_bp.route("/data-normalization/<int:id>", methods=["POST"])(
     login_required(dataNormalization)
+)
+preprocessing_bp.route("/data-reduction/<int:id>", methods=["POST"])(
+    login_required(dataReduction)
 )
