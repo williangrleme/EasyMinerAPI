58d410cd03bda7fdb821058331730e3286887faa
Willian Leme <willian_leme@proton.me>
2024-10-22 23:05:00 -0300

    (fix): Corrige erro de permissionamento

--- api/app/routes/dataset_routes.py
+++ api/app/routes/dataset_routes.py
@@ -12,6 +12,6 @@ dataset_bp = Blueprint("datasets", __name__)
 
 dataset_bp.route("/", methods=["GET"])(get_datasets)
 dataset_bp.route("/<int:id>", methods=["GET"])(login_required(get_dataset))
-dataset_bp.route("/", methods=["POST"])(create_dataset)
+dataset_bp.route("/", methods=["POST"])(login_required(create_dataset))
 dataset_bp.route("/<int:id>", methods=["PUT"])(login_required(update_dataset))
 dataset_bp.route("/<int:id>", methods=["DELETE"])(login_required(delete_dataset))
