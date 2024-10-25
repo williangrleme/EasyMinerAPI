cb16368b121ec630c00d78e42e6de11fd8bdab24
Willian Leme <willian_leme@proton.me>
2024-10-25 00:48:16 -0300

    (fix): remove user_master

--- api/app/config.py
+++ api/app/config.py
@@ -5,7 +5,6 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    USER_MASTER = int(os.getenv("USER_MASTER"))
     SWAGGER = {"title": "EasyMinerAPI", "uiversion": 3}
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # Limite de 16MB
     S3_KEY = os.getenv("S3_KEY")
