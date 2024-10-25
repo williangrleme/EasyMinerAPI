41e6243a514a1c7d160c76098ef3f095b9fa117d
Willian Leme <willian_leme@proton.me>
2024-10-25 00:49:01 -0300

    (fix): corrige erros de nomenclatura em rotas

--- api/app/routes/data_minig_routes/preprocessing_routes.py
+++ api/app/routes/data_minig_routes/preprocessing_routes.py
@@ -1,23 +1,23 @@
 from flask import Blueprint
 from flask_login import login_required
 from app.controllers.data_mining.preprocessing.data_cleaning_controller import (
-    dataCleaning,
+    data_cleaning,
 )
 from app.controllers.data_mining.preprocessing.data_normalization_controller import (
-    dataNormalization,
+    data_normalization,
 )
 from app.controllers.data_mining.preprocessing.data_reduction_controller import (
-    dataReduction,
+    data_reduction,
 )
 
 preprocessing_bp = Blueprint("preprocessing", __name__)
 
 preprocessing_bp.route("/data-cleaning/<int:id>", methods=["POST"])(
-    login_required(dataCleaning)
+    login_required(data_cleaning)
 )
 preprocessing_bp.route("/data-normalization/<int:id>", methods=["POST"])(
-    login_required(dataNormalization)
+    login_required(data_normalization)
 )
 preprocessing_bp.route("/data-reduction/<int:id>", methods=["POST"])(
-    login_required(dataReduction)
+    login_required(data_reduction)
 )
