9dcf2c6b639cea1677f51a00a02c505e3c7a5035
Willian Leme <willian_leme@proton.me>
2024-11-02 22:12:51 -0300

    (fix): remove id como parametro para atualizacao e exclusão de usário

--- api/app/routes/user_routes.py
+++ api/app/routes/user_routes.py
@@ -9,5 +9,5 @@ from app.controllers.user_controller import (
 user_bp = Blueprint("users", __name__)
 
 user_bp.route("/", methods=["POST"])(create_user)
-user_bp.route("/<int:user_id>", methods=["PUT"])(login_required(update_user))
-user_bp.route("/<int:user_id>", methods=["DELETE"])(login_required(delete_user))
+user_bp.route("/", methods=["PUT"])(login_required(update_user))
+user_bp.route("/", methods=["DELETE"])(login_required(delete_user))
