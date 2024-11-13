948d00cb4e318fc066d6fc6981e8aded09936843
Willian Leme <willian_leme@proton.me>
2024-11-12 22:39:33 -0300

    (feat): atualiza documentaçao

--- api/app/config.py
+++ api/app/config.py
@@ -1,11 +1,9 @@
 import os
 
-
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SWAGGER = {"title": "EasyMinerAPI", "uiversion": 3}
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # Limite de 16MB
     S3_KEY = os.getenv("S3_KEY")
     S3_BUCKET = os.getenv("S3_BUCKET")
@@ -22,3 +20,8 @@ class Config:
     WTF_CSRF_ENABLED = False  # TODO: Tratar CSRF futuramente
     SESSION_COOKIE_SAMESITE = "None"
     SESSION_COOKIE_SECURE = True
+    SWAGGER = {
+        "title": "EasyMinerAPI",
+        "uiversion": 3,
+        "openapi": "3.0.0",
+    }
