cfd87c3dacd1f2e1f6b14e041fbbffba9fa7ffa6
Willian Leme <willian_leme@proton.me>
2025-02-06 23:22:02 -0300

    (feat): adiciona distribuição de frequencia ao projeto

--- api/app/routes/__init__.py
+++ api/app/routes/__init__.py
@@ -3,6 +3,8 @@ from flask import Blueprint
 
 def init_routes(app):
     from app.routes.auth_routes import auth_bp
+    from app.routes.data_minig_routes.data_visualization_routes import \
+        data_visualization_bp
     from app.routes.data_minig_routes.preprocessing_routes import \
         preprocessing_bp
     from app.routes.dataset_routes import dataset_bp
@@ -14,3 +16,4 @@ def init_routes(app):
     app.register_blueprint(project_bp, url_prefix="/api/projects")
     app.register_blueprint(dataset_bp, url_prefix="/api/datasets")
     app.register_blueprint(preprocessing_bp, url_prefix="/api/preprocessing")
+    app.register_blueprint(data_visualization_bp, url_prefix="/api/data_visualization")
