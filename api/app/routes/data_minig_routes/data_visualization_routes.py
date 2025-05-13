fe1fd5baea1bfa6567069381fdc998fa36e0ca82
Willian Leme <willian_leme@proton.me>
2025-05-12 22:27:22 -0300

    (fix): corrige url

--- api/app/routes/data_minig_routes/data_visualization_routes.py
+++ api/app/routes/data_minig_routes/data_visualization_routes.py
@@ -3,8 +3,8 @@ from app.controllers.data_mining.data_visualization_controller import \
 from flask import Blueprint
 from flask_login import login_required
 
-data_visualization_bp = Blueprint("data_visualization", __name__)
+data_visualization_bp = Blueprint("data-visualization", __name__)
 
 data_visualization_bp.route(
-    "/frequency_distribution/<int:dataset_id>", methods=["POST"]
+    "/frequency-distribution/<int:dataset_id>", methods=["POST"]
 )(login_required(frequency_distribution))
