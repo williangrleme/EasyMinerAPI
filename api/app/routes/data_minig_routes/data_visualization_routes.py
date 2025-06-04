88ba5d85144bfdc0d83c1d685b9da8a3c34ff151
Willian Leme <willian_leme@proton.me>
2025-06-03 22:48:34 -0300

    (feat): cria rota de moda

--- api/app/routes/data_minig_routes/data_visualization_routes.py
+++ api/app/routes/data_minig_routes/data_visualization_routes.py
@@ -1,5 +1,5 @@
-from app.controllers.data_mining.data_visualization_controller import \
-    frequency_distribution
+from app.controllers.data_mining.data_visualization_controller import (
+    frequency_distribution, mode)
 from flask import Blueprint
 from flask_login import login_required
 
@@ -8,3 +8,7 @@ data_visualization_bp = Blueprint("data-visualization", __name__)
 data_visualization_bp.route(
     "/frequency-distribution/<int:dataset_id>", methods=["POST"]
 )(login_required(frequency_distribution))
+
+data_visualization_bp.route("/mode/<int:dataset_id>", methods=["POST"])(
+    login_required(mode)
+)
