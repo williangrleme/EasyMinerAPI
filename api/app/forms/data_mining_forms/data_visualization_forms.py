6be0b7757907961a1946f98748d4c57240033e78
Willian Leme <willian_leme@proton.me>
2025-06-14 00:43:53 -0300

    (feat): refatora visualização de dados para suportar múltiplos métodos e atualiza documentação

--- api/app/forms/data_mining_forms/data_visualization_forms.py
+++ api/app/forms/data_mining_forms/data_visualization_forms.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, FieldList, StringField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
 
 
 class DataVisualizationForm(FlaskForm):
@@ -10,7 +10,14 @@ class DataVisualizationForm(FlaskForm):
         "column_access": "Não foi possível acessar as colunas da base de dados.",
         "invalid_features": "Os seguintes campos não estão registrados na sua base de dados: {}",
         "invalid_boolean": "O valor deve ser True ou False.",
+        "invalid_method": "Método de visualização '{}' não é válido. Métodos válidos: {}"
     }
+
+    VALID_VISUALIZATION_METHODS = [
+        'frequency_distribution', 'mode', 'midpoint', 'median',
+        'weighted_average', 'mean_frequency_distribution',
+        'geometric_mean', 'harmonic_mean'
+    ]
 
     features = FieldList(
         StringField(
@@ -19,6 +26,11 @@ class DataVisualizationForm(FlaskForm):
     )
 
     use_clean_dataset = BooleanField("Usar dataset Limpo")
+
+    visualization_method = StringField(
+        "Método de Visualização",
+        validators=[DataRequired(message=ERROR_MESSAGES["required"])]
+    )
 
     def __init__(self, file_url: str, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -43,6 +55,9 @@ class DataVisualizationForm(FlaskForm):
         if not self.check_features():
             return False
 
+        if not self.check_visualization_method():
+            return False
+
         return True
 
     def check_features(self):
@@ -55,6 +70,17 @@ class DataVisualizationForm(FlaskForm):
             self.features.errors.append(
                 self.ERROR_MESSAGES["invalid_features"].format(
                     ", ".join(invalid_features)
+                )
+            )
+            return False
+        return True
+
+    def check_visualization_method(self):
+        if self.visualization_method.data not in self.VALID_VISUALIZATION_METHODS:
+            self.visualization_method.errors.append(
+                self.ERROR_MESSAGES["invalid_method"].format(
+                    self.visualization_method.data,
+                    ", ".join(self.VALID_VISUALIZATION_METHODS)
                 )
             )
             return False
