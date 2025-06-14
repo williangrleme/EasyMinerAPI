44f368e5537dfc3e8806cd7025eaafcf279f8254
Willian Leme <willian_leme@proton.me>
2025-06-14 00:43:53 -0300

    (feat): refatora visualização de dados para suportar múltiplos métodos e atualiza documentação

--- api/app/routes/data_minig_routes/data_visualization_routes.py
+++ api/app/routes/data_minig_routes/data_visualization_routes.py
@@ -1,42 +1,9 @@
-from app.controllers.data_mining.data_visualization_controller import (
-    frequency_distribution, mean_frequency_distribution, median, midpoint,
-    mode, weighted_average, geometric_mean, harmonic_mean)
+from app.controllers.data_mining.data_visualization_controller import (data_visualization)
 from flask import Blueprint
 from flask_login import login_required
 
 data_visualization_bp = Blueprint("data-visualization", __name__)
 
 data_visualization_bp.route(
-    "/frequency-distribution/<int:dataset_id>", methods=["POST"]
-)(login_required(frequency_distribution))
-
-data_visualization_bp.route("/mode/<int:dataset_id>", methods=["POST"])(
-    login_required(mode)
-)
-
-data_visualization_bp.route("/midpoint/<int:dataset_id>", methods=["POST"])(
-    login_required(midpoint)
-)
-
-data_visualization_bp.route("/median/<int:dataset_id>", methods=["POST"])(
-    login_required(median)
-)
-
-
-data_visualization_bp.route("/weighted-average/<int:dataset_id>", methods=["POST"])(
-    login_required(weighted_average)
-)
-
-data_visualization_bp.route(
-    "/mean-frequency-distribution/<int:dataset_id>", methods=["POST"]
-)(login_required(mean_frequency_distribution))
-
-data_visualization_bp.route(
-    "/geometric-mean/<int:dataset_id>", methods=["POST"]
-)(login_required(geometric_mean))
-
-data_visualization_bp.route(
-    "/harmonic-mean/<int:dataset_id>", methods=["POST"]
-)(login_required(harmonic_mean))
-
-
+    "/visualize/<int:dataset_id>", methods=["POST"]
+)(login_required(data_visualization))
