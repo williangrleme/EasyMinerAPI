277310447c89ff8f14cf7c47ad9e9cc3e6e63f1c
Willian Leme <willian_leme@proton.me>
2025-07-03 23:01:57 -0300

    (feat): cria funcionalidades de medidas de associação

--- api/app/routes/data_minig_routes/data_visualization_routes.py
+++ api/app/routes/data_minig_routes/data_visualization_routes.py
@@ -1,6 +1,6 @@
 from app.controllers.data_mining.data_visualization_controller import (
-    measure_central_tendency, dispersion_measure, shape_measure
-)
+    association_measure, dispersion_measure, measure_central_tendency,
+    shape_measure)
 from flask import Blueprint
 from flask_login import login_required
 
@@ -10,10 +10,14 @@ data_visualization_bp.route(
     "/measure-central-tendency/<int:dataset_id>", methods=["POST"]
 )(login_required(measure_central_tendency))
 
-data_visualization_bp.route(
-    "/dispersion-measure/<int:dataset_id>", methods=["POST"]
-)(login_required(dispersion_measure))
+data_visualization_bp.route("/dispersion-measure/<int:dataset_id>", methods=["POST"])(
+    login_required(dispersion_measure)
+)
 
-data_visualization_bp.route(
-    "/shape-measure/<int:dataset_id>", methods=["POST"]
-)(login_required(shape_measure))
+data_visualization_bp.route("/shape-measure/<int:dataset_id>", methods=["POST"])(
+    login_required(shape_measure)
+)
+
+data_visualization_bp.route("/association-measure/<int:dataset_id>", methods=["POST"])(
+    login_required(association_measure)
+)
